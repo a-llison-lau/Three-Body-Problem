c2b6d920eee50f0c76dfec6cf709f47644ebc488
a-llison-lau <allison.lau@mail.utoronto.ca>
2025-01-27 13:13:45 -0500

    adjust z index of canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,8 +9,9 @@ function Canvas({ isTextPanelOpen }: CanvasProps) {
   return (
     <div
       className={`fixed top-0 right-0 h-screen transition-all duration-700 ease-in-out ${
-        isTextPanelOpen ? "md:w-5/8" : "w-full"
+        isTextPanelOpen ? "md:w-2/3" : "w-full"
       }`}
+      style={{ zIndex: 0 }}
     >
       {/* Container for ThreeJS */}
       <div className="w-full h-full bg-black">
