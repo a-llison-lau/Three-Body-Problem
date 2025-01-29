de8b3a328ed8359a7da47c6b33ddd8f85aa85f3a
a-llison-lau <allison.lau@mail.utoronto.ca>
2025-01-29 02:42:43 -0500

    fixed author visibility

--- src/components/Authors.tsx
+++ src/components/Authors.tsx
@@ -1,7 +1,10 @@
 function Authors() {
   return (
-    <div className="absolute h-7 w-70 bg-white text-gray-500 right-4 text-right text-xs z-30">
-      <p>By <a href="https://henry-yip.github.io/">@henryyip</a> <a href="https://allison-lau.vercel.app/">@allisonlau</a></p>
+    <div className="fixed h-7 w-70 text-gray-500 right-4 text-right text-xs">
+      <p>
+        By <a href="https://henry-yip.github.io/">@henryyip</a>{" "}
+        <a href="https://allison-lau.vercel.app/">@allisonlau</a>
+      </p>
     </div>
   );
 }
